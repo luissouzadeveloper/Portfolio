a431f32ec7d2b7a232ca5e71c9b7c030b365d9b2
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-08-30 12:34:05 -0300

    Seção de background concluída

---
+++ src/components/Background.jsx
@@ -0,0 +1,12 @@
+const Background = () => {
+    return (
+        <>
+            <section id='background' className="d-flex flex-column justify-content-center text-center">
+                <h1 className="text-white">Luis Felipe</h1>
+                <h2 className="text-primary mt-2 fw-bold font-monospace">Desenvolvedor Web</h2>
+            </section>
+        </>
+    )
+}
+
+export default Background
