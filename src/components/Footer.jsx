884a45562b95db78b4d56d7bea4d4144c25b1e0a
Luis Felipe <luisfelipeosouzati@gmail.com>
2024-09-03 09:49:09 -0300

    Footer concluído

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <>
+            <footer className="bg-primary py-3">
+                <p className="text-center text-white fs-2 fw-bold my-2">Luis Felipe - 2024</p>
+            </footer>
+        </>
+    )
+}
+
+export default Footer
